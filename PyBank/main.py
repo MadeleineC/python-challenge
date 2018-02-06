a395f410bf6d7e487200006901c6ca346b64cc5d
Madeleine Case <Madeleine@Madeleines-MacBook-Pro.local>
2018-02-05 21:29:48 -0500

    Final Version2

--- PyBank/main.py
+++ PyBank/main.py
@@ -32,26 +32,23 @@ budget_data_1 = os.path.join('budget_data_1.csv')
 budget_data_2 = os.path.join('budget_data_2.csv')
 
 ####The total number of months included in the dataset
-# -1 to account for header
-months = 0-1
+months = 0
 
 with open(budget_data_1, newline="") as csvfile:
 	csvreader1 = csv.reader(csvfile, delimiter=",")
+	next (csvfile)
 	for row in csvreader1:
-		if row[1] != "":
-			months = months + 1
+		# if row[1] != "":
+		months = months + 1
 
-with open(budget_data_2, newline="") as csvfile:
-	csvreader1 = csv.reader(csvfile, delimiter=",")
-	for row in csvreader1:
-		if row[1] != "":
-			months = months + 1
+# with open(budget_data_2, newline="") as csvfile:
+# 	csvreader1 = csv.reader(csvfile, delimiter=",")
+# 	next (csvfile)
+#	for row in csvreader1:
+# 		if row[1] != "":
+# 			months = months + 1
 
-# -1 to account for header
-months = months - 1
-print(months)
-
-# print("Total Months: " + str(months))
+# print(months)
 
 ####The total amount of revenue gained over the entire period
 revenue_list = []
@@ -61,11 +58,11 @@ with open(budget_data_1, newline="") as csvfile:
 	next (csvfile)
 	for row in csvreader1:
 		revenue_list.append(int(row[1]))
-with open(budget_data_2, newline="") as csvfile:
-	csvreader1 = csv.reader(csvfile, delimiter=",")
-	next (csvfile)
-	for row in csvreader1:
-		revenue_list.append(int(row[1]))
+# with open(budget_data_2, newline="") as csvfile:
+# 	csvreader1 = csv.reader(csvfile, delimiter=",")
+# 	next (csvfile)
+# 	for row in csvreader1:
+# 		revenue_list.append(int(row[1]))
 total_revenue = sum(revenue_list)
 # print(total_revenue)
 # print(revenue_list)
@@ -77,28 +74,29 @@ for i in range (1, months):
 	change = revenue_list[i] - revenue_list[i-1]
 	change_list.append(change)
 
-average_changes = sum(change_list)/(months-1)
+average_changes = sum(change_list)/len(change_list)
 # print (average_changes)
 
 months_list = []
 with open(budget_data_1, newline="") as csvfile:
 	csvreader1 = csv.reader(csvfile, delimiter=",")
 	next (csvfile)
-	for row in csvreader1:
-		months_list.append(row[0])
-with open(budget_data_2, newline="") as csvfile:
-	csvreader1 = csv.reader(csvfile, delimiter=",")
 	next (csvfile)
 	for row in csvreader1:
 		months_list.append(row[0])
+# with open(budget_data_2, newline="") as csvfile:
+# 	csvreader1 = csv.reader(csvfile, delimiter=",")
+# 	next (csvfile)
+# 	for row in csvreader1:
+# 		months_list.append(row[0])
 
 # print(months_list)
 
 
-max_revenue = max(revenue_list)
-min_revenue = min(revenue_list)
+max_change = max(change_list)
+min_change = min(change_list)
 
-dictionary = dict(zip(months_list, revenue_list))
+dictionary = dict(zip(months_list, change_list))
 
 max_month = max(dictionary, key=dictionary.get)
 min_month = min(dictionary, key=dictionary.get)
@@ -109,8 +107,8 @@ min_month = min(dictionary, key=dictionary.get)
 print('Total Months: ' + str(months))
 print('Total Revenue: ' + "${:}".format(total_revenue))
 print('Average Revenue Change: ' + str(average_changes))
-print('Greatest Increase in Revenue: ' + max_month + ' (' + str("${:}".format(max_revenue)) + ')')
-print('Greatest Decrease in Revenue: ' + min_month + ' (' + str("${:}".format(min_revenue)) + ')')
+print('Greatest Increase in Revenue: ' + max_month + ' (' + str("${:}".format(max_change)) + ')')
+print('Greatest Decrease in Revenue: ' + min_month + ' (' + str("${:}".format(min_change)) + ')')
 
 
 
